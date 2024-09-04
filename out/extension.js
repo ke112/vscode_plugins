ca502375511c871ea6a4cbf45b54178f681186d4
zhihua.zhang <zhihua.zhang@kunlun-inc.com>
2024-09-04 21:29:36 +0800

    #实现选中当前widget即可包裹整体widget

--- out/extension.js
+++ out/extension.js
@@ -10,10 +10,15 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
-        const selectedText = document.getText(range);
-        const wrappedText = wrapWithLayoutBuilder(selectedText);
+        const fullRange = getFullWidgetRange(document, range);
+        if (!fullRange) {
+            vscode.window.showErrorMessage('Unable to determine widget boundaries');
+            return;
+        }
+        const fullWidgetText = document.getText(fullRange);
+        const wrappedText = wrapWithLayoutBuilder(fullWidgetText);
         editor.edit(editBuilder => {
-            editBuilder.replace(range, wrappedText);
+            editBuilder.replace(fullRange, wrappedText);
         }).then(success => {
             if (success) {
                 vscode.window.showInformationMessage('Widget wrapped with LayoutBuilder');
@@ -36,6 +41,40 @@ class LayoutBuilderActionProvider {
         };
         return [wrapAction];
     }
+}
+function getFullWidgetRange(document, range) {
+    const text = document.getText();
+    const startIndex = document.offsetAt(range.start);
+    const endIndex = document.offsetAt(range.end);
+    // 找到选中文本左侧的第一个字母
+    let widgetStart = startIndex;
+    while (widgetStart > 0 && /[a-zA-Z]/.test(text[widgetStart - 1])) {
+        widgetStart--;
+    }
+    // 找到选中文本之后的第一个左括号
+    let openParenIndex = endIndex;
+    while (openParenIndex < text.length && text[openParenIndex] !== '(') {
+        openParenIndex++;
+    }
+    if (openParenIndex === text.length) {
+        return null; // 没有找到左括号
+    }
+    // 找到匹配的右括号
+    let openParenCount = 1;
+    let closeParenIndex = openParenIndex + 1;
+    while (closeParenIndex < text.length && openParenCount > 0) {
+        if (text[closeParenIndex] === '(') {
+            openParenCount++;
+        }
+        else if (text[closeParenIndex] === ')') {
+            openParenCount--;
+        }
+        closeParenIndex++;
+    }
+    if (openParenCount !== 0) {
+        return null; // 没有找到匹配的右括号
+    }
+    return new vscode.Range(document.positionAt(widgetStart), document.positionAt(closeParenIndex));
 }
 function wrapWithLayoutBuilder(widget) {
     return `LayoutBuilder(
--- package.sh
+++ package.sh
@@ -1,1 +1,21 @@
-yarn run compile && vsce package
+#!/bin/bash
+
+# 读取当前版本号
+current_version=$(node -p "require('./package.json').version")
+
+# 分割版本号
+IFS='.' read -ra version_parts <<<"$current_version"
+
+# 增加最后一个数字
+((version_parts[2]++))
+
+# 组合新版本号
+new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+# 更新 package.json 中的版本号
+sed -i '' "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" package.json
+
+echo "Version updated from $current_version to $new_version"
+
+# 编译并打包，自动回答 "yes"
+yarn run compile && echo "y" | vsce package --no-yarn
