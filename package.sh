1947ba7899b73e242b180b640f6bc9d9572c210d
zhihua.zhang <zhihua.zhang@kunlun-inc.com>
2024-11-11 11:24:50 +0800

    打包命令优化

--- package.sh
+++ package.sh
@@ -36,19 +36,26 @@ if [[ $? -eq 0 ]]; then
     # 使用 Visual Studio Code 的完整路径卸载旧版本的插件
     "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" --uninstall-extension "$extension_id"
 
-    # 等待一下确保卸载完成
-    sleep 1
-
-    # 使用 Visual Studio Code 的完整路径安装新版本
-    "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" --install-extension "$vsix_file"
-
     if [[ $? -eq 0 ]]; then
-        echo "插件安装成功！请完全退出 Visual Studio Code 并重新启动以使更改生效。"
-        # 打开新版本的 .vsix 文件位置
-        # open -R "$vsix_file"
+        echo "插件卸载成功, 即将自动安装新版本"
+        echo
+        # 等待一下确保卸载完成
+        sleep 1
+
+        # 使用 Visual Studio Code 的完整路径安装新版本
+        "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" --install-extension "$vsix_file"
+
+        if [[ $? -eq 0 ]]; then
+            echo "插件安装成功！请完全退出 Visual Studio Code 并重新启动以使更改生效。"
+            # 打开新版本的 .vsix 文件位置
+            # open -R "$vsix_file"
+        else
+            echo "插件安装失败，请检查错误信息。"
+        fi
     else
         echo "插件安装失败，请检查错误信息。"
     fi
+
 else
     echo "打包失败，请检查错误信息。"
 fi
