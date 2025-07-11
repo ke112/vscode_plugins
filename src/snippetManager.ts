743a03a9dfd26e11e065300aefb6f9251070067f
zhihua.zhang <54562841@qq.com>
2025-07-11 18:23:29 +0800

    增加逗号

--- src/snippetManager.ts
+++ src/snippetManager.ts
@@ -80,27 +80,27 @@ export class SnippetManager implements vscode.CompletionItemProvider {
       description: 'await 延迟执行'
     },
     'fedgeInsets only 生成间距': {
-      snippet: `EdgeInsetsDirectional.only(start: 16.w, end: 16.w, top: 16.w, bottom: 16.w)`,
+      snippet: `EdgeInsetsDirectional.only(start: 16.w, end: 16.w, top: 16.w, bottom: 16.w),`,
       description: '生成间距'
     },
     'fedgeInsets all 生成间距': {
-      snippet: `EdgeInsetsDirectional.all(16.w)`,
+      snippet: `EdgeInsetsDirectional.all(16.w),`,
       description: '生成间距'
     },
     'fmargin all 设置外边距': {
-      snippet: `margin: EdgeInsetsDirectional.all(16.w)`,
+      snippet: `margin: EdgeInsetsDirectional.all(16.w),`,
       description: '设置外边距'
     },
     'fmargin only 设置外边距': {
-      snippet: `margin: EdgeInsetsDirectional.only(start: 16.w, end: 16.w, top: 16.w, bottom: 16.w)`,
+      snippet: `margin: EdgeInsetsDirectional.only(start: 16.w, end: 16.w, top: 16.w, bottom: 16.w),`,
       description: '设置外边距'
     },
     'fpadding all 设置内边距': {
-      snippet: `padding: EdgeInsetsDirectional.all(16.w)`,
+      snippet: `padding: EdgeInsetsDirectional.all(16.w),`,
       description: '设置内边距'
     },
     'fpadding only 设置内边距': {
-      snippet: `padding: EdgeInsetsDirectional.only(start: 16.w, end: 16.w, top: 16.w, bottom: 16.w)`,
+      snippet: `padding: EdgeInsetsDirectional.only(start: 16.w, end: 16.w, top: 16.w, bottom: 16.w),`,
       description: '设置内边距'
     },
     'ftop 设置顶部': {
